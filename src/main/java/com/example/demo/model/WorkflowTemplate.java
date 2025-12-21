57a6a9ac5702e438c1c0a71dba25c24bda60e745
bala-amypo <bala@amypo.in>
2025-12-21 18:48:18 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/WorkflowTemplate.java
@@ -0,0 +1,34 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class WorkflowTemplate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String templateName;
+    private String description;
+    private int totalLevels;
+    private boolean active;
+
+    public WorkflowTemplate() {}
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getTemplateName() { return templateName; }
+    public void setTemplateName(String templateName) { this.templateName = templateName; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public int getTotalLevels() { return totalLevels; }
+    public void setTotalLevels(int totalLevels) { this.totalLevels = totalLevels; }
+
+   
+    public boolean isActive() { return active; }
+    public void setActive(boolean active) { this.active = active; }
+}
