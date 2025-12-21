41a489f42f6d23f20cf1fec2f27e621c8cd8988d
bala-amypo <bala@amypo.in>
2025-12-21 14:27:18 +0000

    User commit

--- src/main/java/com/example/demo/service/WorkflowStepConfigService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import java.util.List;
-import com.example.demo.model.WorkflowStepConfig;
-
-public interface WorkflowStepConfigService {
-    WorkflowStepConfig createStep(WorkflowStepConfig config);
-    List<WorkflowStepConfig> getStepsForTemplate(Long templateId);
-}
