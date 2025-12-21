8d59f5290972d1d86f8981b635e88ec16590ec71
bala-amypo <bala@amypo.in>
2025-12-21 09:45:06 +0000

    User commit

--- src/main/java/com/example/demo/model/WorkflowStepConfig.java
+++ src/main/java/com/example/demo/model/WorkflowStepConfig.java
@@ -1,80 +1,80 @@
-// package com.example.demo.model;
+package com.example.demo.model;
 
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-// @Entity
-// public class WorkflowStepConfig {
+@Entity
+public class WorkflowStepConfig {
 
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-//     private long templateId;
-//     private int levelNumber;
-//     private String approverRole;
-//     private boolean finalStep;
-//     private String instructions;
+    private long templateId;
+    private int levelNumber;
+    private String approverRole;
+    private boolean finalStep;
+    private String instructions;
 
-//     public WorkflowStepConfig() {
-//     }
+    public WorkflowStepConfig() {
+    }
 
-//     public WorkflowStepConfig(long templateId, int levelNumber, String approverRole,
-//                               boolean finalStep, String instructions) {
-//         this.templateId = templateId;
-//         this.levelNumber = levelNumber;
-//         this.approverRole = approverRole;
-//         this.finalStep = finalStep;
-//         this.instructions = instructions;
-//     }
+    public WorkflowStepConfig(long templateId, int levelNumber, String approverRole,
+                              boolean finalStep, String instructions) {
+        this.templateId = templateId;
+        this.levelNumber = levelNumber;
+        this.approverRole = approverRole;
+        this.finalStep = finalStep;
+        this.instructions = instructions;
+    }
 
-//     public long getId() {
-//         return id;
-//     }
+    public long getId() {
+        return id;
+    }
 
-//     public void setId(long id) {
-//         this.id = id;
-//     }
+    public void setId(long id) {
+        this.id = id;
+    }
 
-//     public long getTemplateId() {
-//         return templateId;
-//     }
+    public long getTemplateId() {
+        return templateId;
+    }
 
-//     public void setTemplateId(long templateId) {
-//         this.templateId = templateId;
-//     }
+    public void setTemplateId(long templateId) {
+        this.templateId = templateId;
+    }
 
-//     public int getLevelNumber() {
-//         return levelNumber;
-//     }
+    public int getLevelNumber() {
+        return levelNumber;
+    }
 
-//     public void setLevelNumber(int levelNumber) {
-//         this.levelNumber = levelNumber;
-//     }
+    public void setLevelNumber(int levelNumber) {
+        this.levelNumber = levelNumber;
+    }
 
-//     public String getApproverRole() {
-//         return approverRole;
-//     }
+    public String getApproverRole() {
+        return approverRole;
+    }
 
-//     public void setApproverRole(String approverRole) {
-//         this.approverRole = approverRole;
-//     }
+    public void setApproverRole(String approverRole) {
+        this.approverRole = approverRole;
+    }
 
-//     public boolean isFinalStep() {
-//         return finalStep;
-//     }
+    public boolean isFinalStep() {
+        return finalStep;
+    }
 
-//     public void setFinalStep(boolean finalStep) {
-//         this.finalStep = finalStep;
-//     }
+    public void setFinalStep(boolean finalStep) {
+        this.finalStep = finalStep;
+    }
 
-//     public String getInstructions() {
-//         return instructions;
-//     }
+    public String getInstructions() {
+        return instructions;
+    }
 
-//     public void setInstructions(String instructions) {
-//         this.instructions = instructions;
-//     }
-// }
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+}
