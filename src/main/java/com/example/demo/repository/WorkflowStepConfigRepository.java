96a8a565aa3c8114987d38ac9e4edf5865def426
bala-amypo <bala@amypo.in>
2025-12-22 01:18:13 +0000

    User commit

--- src/main/java/com/example/demo/repository/WorkflowStepConfigRepository.java
+++ src/main/java/com/example/demo/repository/WorkflowStepConfigRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.WorkflowStepConfig;
+import com.example.demo.entity.WorkflowStepConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,5 +8,6 @@ import java.util.List;
 public interface WorkflowStepConfigRepository
         extends JpaRepository<WorkflowStepConfig, Long> {
 
-    List<WorkflowStepConfig> findByTemplateIdOrderByLevelNumberAsc(Long templateId);
+    List<WorkflowStepConfig>
+    findByTemplateIdOrderByLevelNumberAsc(Long templateId);
 }
