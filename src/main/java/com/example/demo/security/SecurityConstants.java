d06ed7ab2fdf41e8f51e566c1e18e9e355fe7ad1
bala-amypo <bala@amypo.in>
2025-12-26 13:55:50 +0000

    User commit

--- src/main/java/com/example/demo/security/SecurityConstants.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.security;
-
-public class SecurityConstants {
-    // Secret key for signing JWTs
-    public static final String SECRET = "SecretKeyToGenJWTsWithHighEntropyRequiredForRecentVersions";
-    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-}
