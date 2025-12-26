f1b7ae06f664673e47f806e49254ac84902c38c0
bala-amypo <bala@amypo.in>
2025-12-26 09:01:23 +0000

    User commit

--- src/main/java/com/example/demo/repository/WorkflowStepConfigRepository.java
+++ src/main/java/com/example/demo/repository/WorkflowStepConfigRepository.java
@@ -2,18 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.WorkflowStepConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface WorkflowStepConfigRepository extends JpaRepository<WorkflowStepConfig, Long> {
-
-    // ----------------------------------------------------
-    // Used in TestNG: findByWorkflowTemplateId
-    // ----------------------------------------------------
-    List<WorkflowStepConfig> findByWorkflowTemplateId(Long workflowTemplateId);
-
-    // ----------------------------------------------------
-    // Used when deleting template
-    // ----------------------------------------------------
-    void deleteByWorkflowTemplateId(Long workflowTemplateId);
+    List<WorkflowStepConfig> findByTemplateIdOrderByLevelNumberAsc(Long templateId);
 }
