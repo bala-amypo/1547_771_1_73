6cabf4b204b5491f06942923e0072df5bba024fa
bala-amypo <bala@amypo.in>
2025-12-26 18:21:51 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -21,7 +21,6 @@
 // }
 
 
-
 package com.example.demo.dto;
 
 public class RegisterRequest {
@@ -31,4 +30,15 @@ public class RegisterRequest {
     private String role;
 
     // getters & setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -49,3 +49,45 @@
 //                 .orElseThrow(() -> new RuntimeException("User not found"));
 //     }
 // }
+
+
+
+
+package com.example.demo.service.impl;
+
+import com.example.demo.model.Role;
+import com.example.demo.model.User;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired private UserRepository userRepository;
+    @Autowired private RoleRepository roleRepository;
+    @Autowired private PasswordEncoder passwordEncoder;
+
+    @Override
+    public User registerUser(User user, String roleName) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        Role role = roleRepository.findByName(roleName).orElseThrow();
+        user.getRoles().add(role);
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findByUsernameOrEmail(String value) {
+        return userRepository.findByUsername(value).orElse(
+               userRepository.findByEmail(value).orElse(null));
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
+}
+
