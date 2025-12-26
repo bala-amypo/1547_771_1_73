2b58bfd59d866d78369230648cca2c1be854d3d1
bala-amypo <bala@amypo.in>
2025-12-26 15:34:38 +0000

    User commit

--- src/main/java/com/example/demo/util/DateTimeUtil.java
+++ src/main/java/com/example/demo/util/DateTimeUtil.java
@@ -7,7 +7,6 @@ public class DateTimeUtil {
     private static final String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
     public static String format(LocalDateTime dateTime) {
-        if (dateTime == null) return null;
-        return dateTime.format(DateTimeFormatter.ofPattern(DEFAULT_FORMAT));
+        return dateTime == null ? "" : dateTime.format(DateTimeFormatter.ofPattern(DEFAULT_FORMAT));
     }
 }
