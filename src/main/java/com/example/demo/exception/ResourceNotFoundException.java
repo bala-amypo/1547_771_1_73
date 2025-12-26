a0c30e6d4d0ff222ebf61a56726deb27467f386d
bala-amypo <bala@amypo.in>
2025-12-26 15:13:38 +0000

    User commit

---
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+resourceNotFoundHandler.java
+package com.example.demo.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
+package com.example.demo.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/demo/exception/UnauthorizedException.java
@@ -0,0 +1,9 @@
+Unauthorizedexception.java
+package com.example.demo.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
