b3ed468e7c60743c459129db3d68fa7839486376
bala-amypo <bala@amypo.in>
2025-12-27 08:23:37 +0000

    User commit

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,21 +1,3 @@
-// package com.example.demo.config;
-
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-
-// @Configuration
-// public class WebSecurityConfig {
-
-//     @Bean
-//     public PasswordEncoder passwordEncoder() {
-//         return new BCryptPasswordEncoder();
-//     }
-// }
-
-
-
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
